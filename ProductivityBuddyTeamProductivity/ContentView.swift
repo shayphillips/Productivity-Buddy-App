dfb6ec4a3785a71f20a1f95ba0f19e81cae3da6f
Luke-Sturm <lukejared2004@gmail.com>
2026-03-04 13:17:21 -0600

    Initial Commit

---
+++ ProductivityBuddyTeamProductivity/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ProductivityBuddyTeamProductivity
+//
+//  Created by Luke Sturm on 3/4/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ ProductivityBuddyTeamProductivity/ProductivityBuddyTeamProductivityApp.swift
@@ -0,0 +1,17 @@
+//
+//  ProductivityBuddyTeamProductivityApp.swift
+//  ProductivityBuddyTeamProductivity
+//
+//  Created by Luke Sturm on 3/4/26.
+//
+
+import SwiftUI
+
+@main
+struct ProductivityBuddyTeamProductivityApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
