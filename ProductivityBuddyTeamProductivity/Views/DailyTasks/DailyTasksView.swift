17ca125b8163fef5c72aa7c53c7bbfea600319a0
xanderhl <xanderhl4105@gmail.com>
2026-03-06 12:02:55 -0600

    Fixed Navigation in CharacterView and added DailyTasksView.

---
+++ ProductivityBuddyTeamProductivity/Views/DailyTasks/DailyTasksView.swift
@@ -0,0 +1,18 @@
+//
+//  DailyTasksView.swift
+//  ProductivityBuddyTeamProductivity
+//
+//  Created by Xander Lollis on 3/6/26.
+//
+
+import SwiftUI
+
+struct DailyTasksView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    DailyTasksView()
+}
